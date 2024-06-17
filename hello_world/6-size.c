ce9fe0958cf781043ccef26e4fd54826f278ca9b
bluelightwatcher <9550@holbertonstudents.com>
2024-06-17 15:36:25 +0200

    Script that print number of bytes from sizeof operator

---
+++ hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * Main - entry point
+ * printf - will used to display a value
+ * value from the sizeof operator
+ * return: 0
+ */
+int main(void)
+{
+  printf("Size of a char: %d byte(s)\n", sizeof(char));
+  printf("Size of an int: %d byte(s)\n", sizeof(int));
+  printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+  printf("Size of an long long int: %d byte(s)\n", sizeof(long long int));
+  printf("Size of a float: %d byte(s)\n", sizeof(float));
+
+  return (0);
+}
