d7864328e2af50cabb24d1c304c69c9bcbcb4c94
bluelightwatcher <9550@holbertonstudents.com>
2024-06-18 15:06:55 +0200

    amended to display correct output

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 /**
- * main - runs a while loop to print the alphabet on one line
+ * main - runs multiple while loops to print the alphabets on one line
  * Return: 0
  */
 int main(void)
 {
 char n;
 n = 'A';
-	while ((n <= 'Z') && (n <= 'z'))
+	while (n <= 'Z')
+	{
+		putchar(n);
+		n++;
+	}
+	n = 'a'
+	while (n <= 'z')
 	{
 		putchar(n);
 		n++;
