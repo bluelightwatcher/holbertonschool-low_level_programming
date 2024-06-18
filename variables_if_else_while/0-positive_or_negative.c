1dd6495d45b10105306a047a273de02d7e69758e
bluelightwatcher <9550@holbertonstudents.com>
2024-06-18 11:23:14 +0200

    Adding script with less curly braces

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -19,15 +19,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 		if (n>0)
-	{
-	printf("%d is positive",n);
-		} else if(n==0)
-		{ 
+		printf("%d is positive",n);
+		else if(n==0)
 		printf("%d is equal to zero",n);
-		}
 		else
-		{
 		printf("%d is negative",n);
-		}
+		
 	return (0);
 }
