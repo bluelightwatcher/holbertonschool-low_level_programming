efafa352ebb1cb133cbdd93422318cde2aab5960
bluelightwatcher <9550@holbertonstudents.com>
2024-06-18 15:59:05 +0200

    Adding 5 print numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - runs a for loop to print single digits
+ * Return: 0
+ */
+
+int main(void)
+{
+int i;
+for (i = 0 ; i <= 9 ; i++)
+	{
+	printf("%d", i);
+	}
+return (0);
+}
