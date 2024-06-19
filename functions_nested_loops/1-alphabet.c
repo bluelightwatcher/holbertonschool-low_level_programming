8d9404c222f47e321d65951b5f3265b114ee3d78
bluelightwatcher <9550@holbertonstudents.com>
2024-06-19 15:04:46 +0200

    Adding 1-alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*  print_alphabet - loops over the alphabet
+*  and print each letter on the same line
+*
+* Return: Always 0.
+*/
+void print_alphabet(void)
+{
+	int i;
+	for (i = 'a'; i <= 'z' ; i++)
+	{
+	_putchar(i);
+	}
+	_putchar(\n);
+	return (0);
+}
