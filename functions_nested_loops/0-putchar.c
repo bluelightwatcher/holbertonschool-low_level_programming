0b2e8eeaf75d18196f2c84224ce5b607ccf9c798
bluelightwatcher <9550@holbertonstudents.com>
2024-06-19 14:28:28 +0200

    Program printing _putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,3 +1,22 @@
-# !/ bin / bash
-echo "_putchar
-"
+#include "main.h"
+
+/**
+* main - print putchar_
+*
+* Return: always zerooo
+*/
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+return (0);
+}
