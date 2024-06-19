a156c59b8c5143a6414ba262900a09eddbab6afb
bluelightwatcher <9550@holbertonstudents.com>
2024-06-19 15:34:31 +0200

    Correcting typo in code

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -3,7 +3,7 @@
 *  print_alphabet - loops over the alphabet
 *  and print each letter on the same line
 *
-* Return: Always 0.
+* Return: void
 */
 void print_alphabet(void)
 {
@@ -12,6 +12,5 @@ void print_alphabet(void)
 	{
 	_putchar(i);
 	}
-	_putchar(\n);
-	return (0);
+	_putchar('\n');
 }
