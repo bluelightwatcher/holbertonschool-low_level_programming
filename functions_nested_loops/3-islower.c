8b43cf2e06cecbfb366fe694616b4bdfe7ccdc89
bluelightwatcher <9550@holbertonstudents.com>
2024-06-20 12:53:53 +0200

    Adding programm checking for if variable is a lowercase

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *_islower - checks if the character is in lowercase
+ *@c: is the variable
+ *Return: 0
+ */
+int _islower(c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
