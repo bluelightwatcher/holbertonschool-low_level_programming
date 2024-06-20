8506ddff0b520b44075f2d0edb0c62efb8d6a742
bluelightwatcher <9550@holbertonstudents.com>
2024-06-20 20:59:03 +0200

    Betty style correction

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,10 +1,15 @@
 #include "main.h"
 /**
- *
- *
+ *add - add num1 to num2
+ *the result is num3
+ *@num1: is an integer
+ *@num2: is an integer
+ *Return: num3
  */
+
 int add(int num1, int num2)
 {
 	int num3 = num1 + num2;
+
 	return (num3);
 }
