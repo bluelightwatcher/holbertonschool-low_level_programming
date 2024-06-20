6944e556ae31309926503343979785056c425044
bluelightwatcher <9550@holbertonstudents.com>
2024-06-20 13:08:50 +0200

    Programm checks if input is alpha (not numerical)

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*_islower - checks if the character is in lowercase
+*@c: is the variable
+*Return: 0
+*/
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' )
+		return (1);
+	else
+		return (0);
+}
