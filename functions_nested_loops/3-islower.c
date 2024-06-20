7f1ccf63b049b2ac954fccdfc2f6c506fcbaabf2
bluelightwatcher <9550@holbertonstudents.com>
2024-06-20 13:02:26 +0200

    Adding variable type in prototype

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,11 +1,10 @@
 #include "main.h"
-
 /**
- *_islower - checks if the character is in lowercase
- *@c: is the variable
- *Return: 0
- */
-int _islower(c)
+*_islower - checks if the character is in lowercase
+*@c: is the variable
+*Return: 0
+*/
+int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
 		return (1);
