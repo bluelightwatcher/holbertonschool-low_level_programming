fefd50061db6b15a9ae54a382ffb5c868091257e
bluelightwatcher <9550@holbertonstudents.com>
2024-06-20 20:53:02 +0200

    betty style documentation

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,6 +1,12 @@
 #include "main.h"
+/**
+ *_abs - returns the absolute value
+ *if a is below 0 it is multiply by its inverse
+ *@a: is an integer
+ *Return: a
+ */
 
-int _abs (int a)
+int _abs(int a)
 {
 	if (a < 0)
 	{
