6b2a69f6e393127fe06933b51dff6f4e64948269
bluelightwatcher <9550@holbertonstudents.com>
2024-06-21 09:56:42 +0200

    Fixing putchar issue attempt

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -8,13 +8,13 @@ void times_table(void)
 {
 	int i;
 	int j;
-	int k;
 
 	for (i = 0 ; i <= 9 ; i++)
 	{
 		for (j = 0 ; j <= 9 ; j++)
 		{
-			k = i * j;
+			int k = i * j;
+			_abs (k);
 			_putchar(k);
 			_putchar(',');
 			_putchar(' ');
