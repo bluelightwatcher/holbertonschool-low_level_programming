19e0e90b56ca8b3f097ec0ac30ca8f129f51c01b
bluelightwatcher <9550@holbertonstudents.com>
2024-06-21 12:30:19 +0200

    Programm prints 9 times table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,22 +1,42 @@
 #include "main.h"
-/**
- *times_table - prints the times table of 9
- *Return: void
- */
 
+/**
+*times_table - prints the times table of 9
+*Return: void
+*/
 void times_table(void)
 {
-	int i;
-	int j;
+	int i = 0;
+	int j = 0;
+	
 
 	for (i = 0 ; i <= 9 ; i++)
 	{
 		for (j = 0 ; j <= 9 ; j++)
 		{
+			
 			int k = i * j;
+			if (k <= 9)
+			{
+	       		k = k + 48;
 			_putchar(k);
 			_putchar(',');
 			_putchar(' ');
+			_putchar(' ');
+			}
+			else if (k >9)
+			{
+			k = k / 10;
+			k = k + 48;
+			_putchar (k);
+			k = i * j;
+			k = k % 10;
+			k = k + 48;
+			_putchar(k);
+			_putchar(',');
+			_putchar(' ');
+			}
+			
 		}
 		_putchar ('\n');
 	}
