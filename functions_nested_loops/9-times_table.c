607cd7a64be7a2b1c981be976daa6ba2ef7165f7
bluelightwatcher <9550@holbertonstudents.com>
2024-06-21 09:35:04 +0200

    Programm prints times table of 9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *times_table - prints the times table of 9
+ *Return: void
+ */
+
+void times_table(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 0 ; i <= 9 ; i++)
+	{
+		for (j = 0 ; j <= 9 ; j++)
+		{
+			k = i * j;
+			_putchar(k);
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar ('\n')
+	}
+}
