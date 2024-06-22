b3540ff075d233d3eceb8b8df36648e94161e573
bluelightwatcher <9550@holbertonstudents.com>
2024-06-22 11:47:57 +0200

    Format func match expected output Betty coding style OK documentation TBD

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,24 +1,40 @@
 #include "main.h"
+/**
+ * times_table - prints the time tables from 0 to 9
+ * Return: void
+ */
 
-char result (int k)
+/**
+ * result - calculate the result of i mutliplying j
+ * @k: is an integer
+ * Description: it converts the integer into ascii printable character
+ * Return: the ascii value of an integer between 0 and 99
+ */
+
+/**
+ * format - display the result of the calculation
+ * Return: void
+ */
+
+char result(int k)
 {
 	if (k > 9)
 	{
-	_putchar((k/10)+48);
-	_putchar((k%10)+48);
+	_putchar((k / 10) + 48);
+	_putchar((k % 10) + 48);
 	}
 	else
 	{
-	_putchar(k+48);
+	_putchar(k + 48);
 	}
 return (0);
 }
 
-char format (char k ,int j)
+char format(char k, int j)
 {
-	while (j < 9)
+	while (j > 0)
 	{
-		if (k >9)
+		if (k > 9)
 		{
 		_putchar(',');
 		_putchar(' ');
@@ -33,20 +49,20 @@ char format (char k ,int j)
 	}
 return (0);
 }
-	
-void times_table (void)
+
+void times_table(void)
 {
 	int i;
 	int j;
-	
-	for (i =0 ; i <=9 ; i++)
+
+	for (i = 0 ; i <= 9 ; i++)
 	{
 		for (j = 0 ; j <= 9 ; j++)
 		{
 			int k = i * j;
-		
+
+			format (k, j);
 			result(k);
-			format (k,j);
 		}
 	_putchar('\n');
 	}
