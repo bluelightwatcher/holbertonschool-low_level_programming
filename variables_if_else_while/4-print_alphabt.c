3d9edc030245ec0e39a51f9e8a08ca34e5d30d42
bluelightwatcher <9550@holbertonstudents.com>
2024-06-22 14:02:44 +0200

    Program uses continu statetment to skip letter in the alphabet

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+/**
+ * main - prints the alphabet from a to z
+ * Description: skips the letter e and q
+ * Return: zero
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a' ; letter <= 'z' ; letter++)
+	{
+		if (letter == 'e')
+		{
+			continue;
+		}
+		else if (letter == 'q')
+		{
+			continue;
+		}
+		else
+			putchar(letter);
+	}
+	return (0);
+}
