de26bfd3a8e3e52c26af668d9fd2aa75146252dc
bluelightwatcher <9550@holbertonstudents.com>
2024-06-23 18:47:38 +0200

    Expanding result func to convert numbers over 99 in ascii :(

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -12,8 +12,22 @@ int n;
 
 char result(int k)
 {
-	_putchar(k + '0');
-	return (0);
+	if (k > 9)
+	{
+	_putchar((k / 10) + 48);
+	_putchar((k % 10) + 48);
+	}
+	else if (k > 99)
+	{
+	_putchar((k / 100) + 48);
+	_putchar(((k / 100) % 10) + 48);
+	_putchar(k % 10) + 48);
+	}
+	else
+	{
+	_putchar(k + 48);
+	}
+return (0);
 }
 
 char format(char k, int j)
