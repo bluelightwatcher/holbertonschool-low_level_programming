69bfb3da34a80dbe839308651fccabc735aca142
bluelightwatcher <9550@holbertonstudents.com>
2024-06-23 15:28:37 +0200

    Program 101std librarie

---
+++ functions_nested_loops/101-natural.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ * Return: void
+ */
+
+int main(void)
+{
+	int mult3 = 0;
+	int mult5 = 0;
+	int i;
+
+	for (i = 0; i < 1024 ; i++)
+	{
+		if (i % 3 == 0)
+		{
+			mult3 = (mult3 + i);
+		}
+		else if (i % 5 == 0)
+		{
+			mult5 = (mult5 + i);
+		}
+	}
+	printf("%d\n", mult3 + mult5);
+	putchar('\n');
+}
