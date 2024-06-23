6dea787914dc7e5e9af925744d248066464edb5c
bluelightwatcher <9550@holbertonstudents.com>
2024-06-23 13:42:55 +0200

    Programm prints all hexadecimal character + betty applied

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ *main - entry point
+ *Return: void
+ */
+int main(void)
+{
+	char i;
+	char n;
+
+	for (i = '0' ; i <= '9' ; i++)
+	{
+		putchar(i);
+	}
+	
+	for (n = 'a' ; n <= 'f' ; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+}
