8b46aa2d86e98ea6176c3fbb7b8199c4b00cc8f3
bluelightwatcher <9550@holbertonstudents.com>
2024-06-23 14:49:23 +0200

    Adding condition to program

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,15 +1,28 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
 *print_last_digit - uses modulo to print the last digit
 *@a: is an integer
 *Return: a
 */
+
 int print_last_digit(int a)
 {
-	a = a * -1;
-	a = a % 10;
-	_putchar(a + 48);
-	return (a);
-}
 
+	if (a <= 0)
+	{
+		a = a * -1;
+		a = a % 10;
+		putchar(a + 48);
+		putchar(a + 48);
+		return (a);
+	}
+	else
+	{
+		a = a % 10;
+		putchar(a + 48);
+		putchar(a + 48);
+		return (a);
+	}
+}
