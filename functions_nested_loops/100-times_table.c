6c9af5b2e043f129a1837307e68c5a7184feaa06
bluelightwatcher <9550@holbertonstudents.com>
2024-06-23 17:10:59 +0200

    Program runs time table to n length

---
+++ functions_nested_loops/100-times_table.c
@@ -0,0 +1,63 @@
+#include "main.h"
+/**
+ * times_table - prints time tables from 0 to 9
+ * result - calculate i mutliplying j
+ * @k: is an integer
+ * Description: converts int into ascii character
+ * format - display the result
+ * Return: void
+ */
+
+char result(int k)
+{
+	if (k > 9)
+	{
+	_putchar((k / 10) + 48);
+	_putchar((k % 10) + 48);
+	}
+	else
+	{
+	_putchar(k + 48);
+	}
+return (0);
+}
+
+char format(char k, int j)
+{
+	while (j > 0)
+	{
+		if (k > 9)
+		{
+		_putchar(',');
+		_putchar(' ');
+		}
+		else
+		{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		}
+	break;
+	}
+return (0);
+}
+
+
+
+void print_times_table(int n)
+{
+	int i;
+	int j;
+
+	for (i = 0 ; i <= n ; i++)
+	{
+		for (j = 0 ; j <= n ; j++)
+		{
+			int k = i * j;
+
+			format (k, j);
+			result(k);
+		}
+	_putchar('\n');
+	}
+}
