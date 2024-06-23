8483f1569bb0e8590be64a153af99d4550fc5f9e
bluelightwatcher <9550@holbertonstudents.com>
2024-06-23 17:38:41 +0200

    Ordering function declaration (undeclared value)

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -8,6 +8,28 @@
  * Return: void
  */
 
+void print_times_table(int n)
+{
+	int i;
+	int j;
+
+	if (n > 0 && n < 15)
+	{
+
+		for (i = 0 ; i <= n ; i++)
+		{
+			for (j = 0 ; j <= n ; j++)
+			{
+				int k = i * j;
+
+				format (k, j);
+				result(k);
+			}
+		_putchar('\n');
+		}
+	}
+}
+
 char result(int k)
 {
 	if (k > 9)
@@ -47,28 +69,4 @@ char format(char k, int j)
 	break;
 	}
 return (0);
-}
-
-
-
-void print_times_table(int n)
-{
-	int i;
-	int j;
-
-	if (n > 0 && n < 15)
-	{
-
-		for (i = 0 ; i <= n ; i++)
-		{
-			for (j = 0 ; j <= n ; j++)
-			{
-				int k = i * j;
-
-				format (k, j);
-				result(k);
-			}
-		_putchar('\n');
-		}
-	}
 }
