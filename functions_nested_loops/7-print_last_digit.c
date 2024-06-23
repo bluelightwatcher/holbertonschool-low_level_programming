fb7f366a296cf554e33924f7d7d9bfa63bbc1c7c
bluelightwatcher <9550@holbertonstudents.com>
2024-06-23 14:30:30 +0200

    convert int to absolute putchar ascii value and returning integer

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -3,9 +3,11 @@
 /**
 *print_last_digit - uses modulo to print the last digit
 *@a: is an integer
+*Return: a
 */
 int print_last_digit(int a)
 {
+	a = a * -1;
 	a = a % 10;
 	_putchar(a + 48);
 	return (a);
