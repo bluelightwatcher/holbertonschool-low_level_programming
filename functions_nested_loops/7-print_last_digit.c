3d516d02fcc07c121ba13d20679660b9f949da4d
bluelightwatcher <9550@holbertonstudents.com>
2024-06-23 14:16:28 +0200

    Program should compile with used main.c

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,10 +1,12 @@
 #include "main.h"
+
 /**
 *print_last_digit - uses modulo to print the last digit
 *@a: is an integer
 */
 int print_last_digit(int a)
 {
-	return (a%10);
+	_putchar(a%10);
+	return (0);
 }
 
