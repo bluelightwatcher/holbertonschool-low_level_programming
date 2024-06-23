10c83357fae113276934eed1ae9da7176d462dcb
bluelightwatcher <9550@holbertonstudents.com>
2024-06-23 11:49:09 +0200

    Programm uses stdio.h

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+/**
+ * main - runs the print to 98 program
+ * Return: zero
+ */
+
+int main(void)
+{
+	int n = 98;
+	int target = 98;
+
+	if (n < target)
+	{
+		while (n < (target + 1))
+		{
+			printf("%d, ", n);
+			if (n == 98)
+			{
+				printf("%d\n", n);
+			}
+			n++;
+		}
+	}
+	else if (n > target)
+	{
+		while (n > (target - 1))
+		{
+			printf("%d, ", n);
+			if (n == 98)
+			{
+				printf("%d\n", n);
+			}
+			n--;
+		}
+	}
+	else
+	{
+		printf("%d\n", target);
+	}
+return (0);
+}
