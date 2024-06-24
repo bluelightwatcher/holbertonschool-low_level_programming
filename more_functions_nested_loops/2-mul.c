98514d47160178460ef5a641970da4486111e213
bluelightwatcher <9550@holbertonstudents.com>
2024-06-24 10:45:11 +0200

    Program returns the value of a multiplication

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * mul - multiplies 2 integers
+ * @a: first argument
+ * @b: second argument
+ * Return: c
+ */
+
+int mul(int a, int b)
+{
+	int c = a * b;
+
+	return (c);
+}
