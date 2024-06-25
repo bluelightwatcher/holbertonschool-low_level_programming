6be83ec8f3f8d1a4e88500b11f798ae30ccc922d
bluelightwatcher <9550@holbertonstudents.com>
2024-06-25 15:02:29 +0200

    Program Fizz Buzz ending with no space but a new line

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -9,24 +9,32 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	do {
+		for (i = 1; i < 100; i++)
+		{
+			if (i % 3 == 0 && i % 5 == 0)
+			{
+				printf("FizzBuzz ");
+			}
+			else if (i % 3 == 0)
+			{
+				printf("Fizz ");
+			}
+			else if (i % 5 == 0)
+			{
+				printf("Buzz ");
+			}
+			else
+			{
+				printf("%d ", i);
+			}
+		}
+	} while (i < 100);
+
+	if (i == 100)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
-		{
-			printf("FizzBuzz ");
-		}
-		else if (i % 3 == 0)
-		{
-			printf("Fizz ");
-		}
-		else if (i % 5 == 0)
-		{
-			printf("Buzz ");
-		}
-		else
-		{
-			printf("%d ", i);
-		}
+		printf("Buzz");
 	}
+putchar('\n');
 return (0);
 }
