99b84f0ea4e8b53e6878dea1498262308ef52774
bluelightwatcher <9550@holbertonstudents.com>
2024-06-26 14:10:48 +0200

    Programm uses a loop to count the number of character

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - loops over the string as an array
+ * @s: is a character
+ * Return: final value of i *
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	return (i);
+
+}
