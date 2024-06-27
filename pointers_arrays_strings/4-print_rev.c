f03acebdf9e8a4835a935b65e7db49da5624661b
bluelightwatcher <9550@holbertonstudents.com>
2024-06-27 10:47:04 +0200

    Programm prints a string in reverse

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include"main.h"
+
+void print_rev(char *s)
+{
+	int index;
+
+	for (index = 0; str[index] != '\0'; index++)
+	{
+	}
+	while (index >= 0)
+	{
+		index--;
+		_putchar(str[index]);
+	}
+	_putchar('\n');
+}
