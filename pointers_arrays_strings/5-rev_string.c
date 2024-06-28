79b48ee1568549a5a149588bd7232c0ec9057992
bluelightwatcher <9550@holbertonstudents.com>
2024-06-28 23:32:17 +0200

    The programm swaps value using a variable not a pointer (implicit from arrays)

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,23 +1,31 @@
 #include"main.h"
 
+
 /**
  * rev_string - loops over the string in reverse
  * Description: first goes to the end of the string
  * then prints characters whilst going back to the first character
- * once the loop is over a null character 
+ * once the loop is over a null character
  * @s: is a string
  */
 
 void rev_string(char *s)
 {
-	int index;
-
-	for (index = 0; s[index] != '\0'; index++)
+	int end;
+	int start = 0;
+	char temp;
+	
+	for (end = 0; s[end] != '\0'; end++)
 	{
 	}
-	while (index > 0)
+	end--;
+	
+	while (start < end)
 	{
-		index--;
-		_putchar(s[index]);
+		temp = s[start];
+		s[start] = s[end];
+		s[end] = temp;
+		start++;
+		end--;
 	}
 }
