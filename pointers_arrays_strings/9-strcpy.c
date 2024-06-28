308d6516c099d81fecf480e58c250fed65e0096c
bluelightwatcher <9550@holbertonstudents.com>
2024-06-28 17:00:35 +0200

    betty documentation applied

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -10,15 +10,15 @@
 char *_strcpy(char *dest, char *src)
 {
 	char *start = dest;
-	
+
 	while (*src != '\0')
-	{ 
+	{
 		*dest = *src;
-		src ++;
+		src++;
 		dest++;
 	}
 	*dest = '\0';
 
 
-	return(start);
+	return (start);
 }
