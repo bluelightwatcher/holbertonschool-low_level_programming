e8f6d81855b7985926293c3dae6eacd408898d78
bluelightwatcher <9550@holbertonstudents.com>
2024-06-28 16:29:08 +0200

    Extending first logic

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,10 +1,16 @@
 #include"main.h"
 #include<stdio.h>
+/**
+ * _strcpy - copies one string to a destination
+ * @dest: is an array of characters
+ * @src: is an array of characters
+ * Return: dest
+ */
 
 char *_strcpy(char *dest, char *src)
 {
 	
-	dest = src;
+	dest[] = src;
 
 
 	return(dest);
