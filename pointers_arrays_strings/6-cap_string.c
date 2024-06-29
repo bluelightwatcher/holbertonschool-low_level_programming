7a084c0dcfdcd3bbeb9bbef489019a575161dd89
bluelightwatcher <9550@holbertonstudents.com>
2024-06-29 22:26:27 +0200

    Programm prints the first letter of each word in a string

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include"main.h"
+#include<stdio.h>
+/**
+ * cap_string - capitalize the first letter of a word in a string
+ * @str: is a string
+ * Return: str
+ */
+char *cap_string(char *str)
+{
+	char *temp = str;
+
+	while (temp == str)
+	{
+		if (*temp >= 'a' && *temp <= 'z')
+		{
+			*temp = *temp - 32;
+			break;
+		}
+	temp++;
+	}
+	while (*temp != '\0')
+	{
+		if (*temp == ' ' || *temp == '\n' || *temp == '\t')
+		{
+			temp++;
+			if (*temp >= 'a' && *temp <= 'z')
+			{
+				*temp = *temp - 32;
+			}
+		}
+	temp++;
+	}
+return (str);
+}
