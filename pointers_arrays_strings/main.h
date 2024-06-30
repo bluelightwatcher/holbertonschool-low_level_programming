f1b5aac7d3d185b2f1f9baa8cd8702a2a761a644
bluelightwatcher <9550@holbertonstudents.com>
2024-06-30 09:39:29 +0200

    Updating header file

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -4,6 +4,7 @@
 char *cap_string(char *);
 char *string_toupper(char *);
 int _strcmp(char *s1, char *s2);
+char *_strncat(char *dest, char *src, int n);
 char *_strcat(char *dest, char *src);
 int _atoi(char *s);
 char *_strcpy(char *dest, char *src);
