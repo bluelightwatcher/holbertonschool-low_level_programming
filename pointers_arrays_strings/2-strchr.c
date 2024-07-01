1ee2826f2f5b736b7e7337f04cb9c8aff893cc43
bluelightwatcher <9550@holbertonstudents.com>
2024-07-01 12:49:37 +0200

    Programm scans for a character in a string

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include"main.h"
+
+/**
+ *_strchr - looks for a character in a string
+ *@s: is a string
+ *@c: is a character
+ *Return: ptr if C is found NULL if not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	char *ptr = s;
+
+	while (*ptr != '\0')
+	{
+		if (*ptr == c)
+		{
+			return (ptr);
+		}
+		ptr++;
+	}
+return (NULL);
+}
