d9898b7fd717d9ee691f252fcf9d799ad9d92f9a
bluelightwatcher <9550@holbertonstudents.com>
2024-07-02 09:28:24 +0200

    Program uses a do while loop

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include"main.h"
+/**
+ * _strpbrk - look for matching value of accept in s
+ * @s: is an array of charachters
+ * @accept: is an array of charachters
+ * Return: pointer to matching value or NULL pointer
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	char *start = accept;
+
+	do {
+		while (*accept != '\0')
+		{
+			if (*accept == *s)
+			{
+				return (s);
+			}
+			accept++;
+		}
+	accept = start;
+	s++;
+	} while (*s != '\0');
+
+	return (NULL);
+}
