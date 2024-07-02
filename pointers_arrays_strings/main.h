0833246fc71dfc5779104bd609aaaaf030f7542c
bluelightwatcher <9550@holbertonstudents.com>
2024-07-02 09:27:34 +0200

    Adding proto strpbrk to header

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef MYHEADER
 #define MYHEADER
 
+char *_strpbrk(char *s, char *accept);
 unsigned int _strspn(char *s, char *accept);
 char *_strchr(char *s, char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
