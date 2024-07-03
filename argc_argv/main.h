1ada55050b04b31c8380afe9565ca0462c499167
bluelightwatcher <9550@holbertonstudents.com>
2024-07-03 11:05:24 +0200

    Adding repo and first use of argc&argv

---
+++ argc_argv/main.h
@@ -0,0 +1,29 @@
+#ifndef MYHEADER
+#define MYHEADER
+
+char *_strpbrk(char *s, char *accept);
+unsigned int _strspn(char *s, char *accept);
+char *_strchr(char *s, char c);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_memset(char *s, char b, unsigned int n);
+char *_strncpy(char *dest, char *src, int n);
+void reverse_array(int *a, int n);
+char *leet(char *);
+char *cap_string(char *);
+char *string_toupper(char *);
+int _strcmp(char *s1, char *s2);
+char *_strncat(char *dest, char *src, int n);
+char *_strcat(char *dest, char *src);
+int _atoi(char *s);
+char *_strcpy(char *dest, char *src);
+void print_array(int *a, int n);
+int _strlen(char *s);
+void puts_half(char *str);
+void puts2(char *str);
+void rev_string(char *s);
+void print_rev(char *s);
+void _puts(char *str);
+void swap_int(int *a, int *b);
+void reset_to_98(int *n);
+int _putchar(char c);
+#endif
