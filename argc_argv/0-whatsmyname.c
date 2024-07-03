31a7361060fa671bb41058523b51413575ad21af
bluelightwatcher <9550@holbertonstudents.com>
2024-07-03 12:13:32 +0200

    Program should run using the attribute unused

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,7 +1,15 @@
 #include"main.h"
 #include<stdio.h>
 
-int main(int argc, char *argv[])
+/**
+ * main - program examplifies the use of argc and argv
+ * @argc: is an integer
+ * @argv: is an array of character
+ * Return: zero
+ */
+
+int main(int __attribute__((unused)) argc, char *argv[])
 {
 	printf("%s", argv[0]);
+	return (0);
 }
