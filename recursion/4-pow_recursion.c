541920bbbbf63ac92c6a26f45de02d62a572194e
bluelightwatcher <9550@holbertonstudents.com>
2024-07-04 12:31:26 +0200

    Program calculate x to the power of y

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include"main.h"
+#include<stdio.h>
+
+/**
+ * _pow_recursion - calculate x to the factor of y recursively
+ * @x: is an integer
+ * @y: is an integer
+ * Return: -1 or "x" or "x" * "x"
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 1 || y == 0)
+	{
+		return (x);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
+return (0);
+}
