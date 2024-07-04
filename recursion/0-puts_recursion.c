3bb829c1cc506cec7008553ab23dbb842cfb90e3
bluelightwatcher <9550@holbertonstudents.com>
2024-07-04 11:26:45 +0200

    Programm prints strings recursively (also prints null byte)

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include"main.h"
+
+/**
+ * _puts_recursion - prints characters till the end of a string recursively
+ * @s: is a pointer to a character
+ * Return:  s + 1 or nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\0');
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	return (_puts_recursion(s + 1));
+}
+
+
