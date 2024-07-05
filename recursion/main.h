3b116da8efb4fc08d7c17ceba2672a461a9ba849
bluelightwatcher <9550@holbertonstudents.com>
2024-07-05 21:21:10 +0200

    updating header file

--- recursion/main.h
+++ recursion/main.h
@@ -1,10 +1,10 @@
 #ifndef MYHEADER
 #define MYHEADER
 
+int _strlen_recursion(char *s);
 int is_prime(unsigned int n, unsigned int c);
 int is_prime_number(int n);
 void _print_rev_recursion(char *s);
-
 int _putchar(char c);
 int _sqrt_recursion(int n);
 int _pow_recursion(int x, int y);
