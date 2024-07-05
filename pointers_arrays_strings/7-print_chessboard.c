e79af9683ef12fbdf470d8de3c8bd6875ad40944
bluelightwatcher <9550@holbertonstudents.com>
2024-07-05 10:00:31 +0200

    Program prints a chessboard

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,25 @@
+#include"main.h"
+
+/**
+ * print_chessboard - uses nested while loop to print a 2 dim array
+ * @a: is a 2 dim array
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+
+	int col = 0;
+
+	while (col < 8)
+	{
+		int line = 0;
+
+		while (line < 8)
+		{
+			_putchar(a[col][line]);
+		}
+	_putchar('\n');
+	col++;
+	}
+}
