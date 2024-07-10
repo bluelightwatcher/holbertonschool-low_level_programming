43c31c63db46443c5366d4f0b2e2159c659ec538
bluelightwatcher <9550@holbertonstudents.com>
2024-07-10 09:12:26 +0200

    Applied betty linter

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,28 +1,35 @@
 #include "main.h"
 #include<stdlib.h>
 
+/**
+ * create_array - function creates an array by dynamically allocating memory
+ * @size: is an unsigned int (defines number of bytes)
+ * @c: is a character to print into each byte
+ * Return: pointer to char
+ */
+
 
 char *create_array(unsigned int size, char c)
 {
-	if (size == 0) //checks if size is null
+	if (size == 0) /* checks if size is null */
 	{
 		return (NULL);
 	}
-	
-	char *temp = (char*)malloc(sizeof(unsigned int) * size);
 
-	if (temp == NULL) //checks if malloc failled
+	char *temp = (char *)malloc(sizeof(unsigned int) * size);
+
+	if (temp == NULL) /* checks if malloc failled */
 	{
 		return (NULL);
 	}
 
 	unsigned int counter = 0;
 
-	while (counter < size) // initialize the array with c
+	while (counter < size) /*initialize the array with c */
 	{
 		temp[counter] = c;
 		counter++;
 	}
-	return (temp); // Returns the temp pointer
+	return (temp); /* Returns the temp pointer */
 }
 
