8d817eb92709fdd75ca62d76a352def0d59d59b0
bluelightwatcher <9550@holbertonstudents.com>
2024-07-11 12:29:43 +0200

    Adding folder and first task of structure project

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef MYSTRUCT
+#define MYSTRUCT
+/**
+ * struct dog - structure contains elements related to a dog
+ * @name: pointer to dog's name
+ * @age: pointer to dog's age
+ * @owner: pointer to dog owner's name
+ */
+
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+};
+#endif
