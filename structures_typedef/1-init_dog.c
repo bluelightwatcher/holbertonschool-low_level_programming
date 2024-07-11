4cc06d9bdfaa3fffda94546b82cc6dfec3c5f379
bluelightwatcher <9550@holbertonstudents.com>
2024-07-11 15:21:11 +0200

    Adjusting return void

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,4 +1,4 @@
-#include"dog.h"
+#include "dog.h"
 /**
  * init_dog - function initializes the struct with pointer
  * @d: is a pointer to struct dog
@@ -10,6 +10,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == 0)
+	{
+		return;
+	}
 
 	d->name = name;
 	d->age = age;
