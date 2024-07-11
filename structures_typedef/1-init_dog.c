5d42b4c16f1b0c3fe69338d75d1e9a419a74f480
bluelightwatcher <9550@holbertonstudents.com>
2024-07-11 14:14:05 +0200

    Program initialiazes a structure with pointers

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include"dog.h"
+/**
+ * init_dog - function initializes the struct with pointer
+ * @d: is a pointer to struct dog
+ * @name: is a pointer to variable name
+ * @age: is a float
+ * @owner: is a pointer to variable owner
+ */
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+	{
+		return (NULL);
+	}
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
