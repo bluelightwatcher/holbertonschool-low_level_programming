30899a4ff8acd288af192809f643c022179f92fb
bluelightwatcher <9550@holbertonstudents.com>
2024-07-11 15:04:37 +0200

    First draft to check if it compiles

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "dog.h"
+
+void print_dog(struct dog *d)
+{
+	if (name == NULL)
+	{
+		printf("Name: (nil)")
+	}
+	else
+	{
+		printf("Name: %s", dog->name);
+	}
+
+	if (age == NULL)
+	{
+		printf("\nAge: (nil)")
+	}
+	else
+	{
+		printf("\nAge:%f ", dog->age);
+	}
+
+	if (owner == NULL)
+	{
+		printf("\nOwner: (nil)")
+	}
+	else
+	{
+		printf("\nOwner: %s", dog->owner);
+	}
+}
