dc015f08f01ea1aeeb307c31a8c63a984607af02
bluelightwatcher <9550@holbertonstudents.com>
2024-07-12 10:27:49 +0200

    using other header to check for compiling error

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,11 +1,35 @@
-#ifndef MYSTRUCT
+#ifndef TOUTOU_H
+#define TOUTOU_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+/**
+ * struct dog - infos sur toutou
+ * @name: nom chien
+ * @age: age clebs
+ * @owner: proprio du boudin
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+typedef struct dog dog_t;
+	void	init_dog(struct dog *d, char *name, float age, char *owner);
+	void	print_dog(struct dog *d);
+	dog_t	*new_dog(char *name, float age, char *owner);
+	void	free_dog(dog_t *d);
+#endif
+
+//*#ifndef MYSTRUCT
 #define MYSTRUCT
 /**
  * struct dog - structure contains elements related to a dog
  * @name: pointer to dog's name
  * @age: pointer to dog's age
  * @owner: pointer to dog owner's name
- */
+ *
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
@@ -24,3 +48,4 @@ typedef struct dog
 } dog_t;
 
 #endif
+*/
