444cab2e9ba9400db215986edd61147eb0da48c6
bluelightwatcher <9550@holbertonstudents.com>
2024-07-12 22:30:38 +0200

    Programm frees the pointer using a for loop for each column

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,5 +1,13 @@
-#include <main.h>
+#include "main.h"
 #include <stdlib.h>
+
+/**
+ * free_grid - functions frees a 2D array
+ * @grid: is a pointer to a pointer to int (row)
+ * @height: is an integer (column)
+ * Return: void
+ */
+
 
 void free_grid(int **grid, int height)
 
