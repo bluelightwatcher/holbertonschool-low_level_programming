f7225e2983af6a5e001a097ff7f691162e9cf661
bluelightwatcher <9550@holbertonstudents.com>
2024-07-12 19:22:30 +0200

    Choose the right function helps

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -19,31 +19,25 @@ int _strlen(char *s)
 }
 
 /**
- *_strncpy - copy characters from src to dest
+ *_strcpy - copy characters from src to dest
  *@dest: is a pointer
  *@src: is a pointer
- *@n: is an integer
  *Return: pointer start
  */
 
-char *_strncpy(char *dest, char *src, int n)
+char *_strcpy(char *dest, char *src)
 {
 	char *start = dest;
 
-	while (n > 0 && *src != '\0')
+	while (*src != '\0')
 	{
 		*dest = *src;
 		src++;
 		dest++;
-		n--;
 	}
-	while (n > 0)
-	{
-		*dest = '\0';
-		dest++;
-		n--;
-	}
-return (start);
+	*dest = '\0';
+
+	return (start);
 }
 
 /**
