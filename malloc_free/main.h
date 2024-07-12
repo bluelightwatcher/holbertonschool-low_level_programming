cfaf9c7c15dc22b961a68e81f227c89da9bfd4de
bluelightwatcher <9550@holbertonstudents.com>
2024-07-12 18:34:02 +0200

    Updating header file

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -1,5 +1,8 @@
 #ifndef MYHEADER
 #define MYHEADER
+#include<stdlib.h>
+
+int **alloc_grid(int width, int height);
 int _strlen(char *s);
 char *_strdup(char *str);
 char *create_array(unsigned int size, char c);
