edb3c18a9309b55e5409b75c5479e560d5a5ad86
bluelightwatcher <9550@holbertonstudents.com>
2024-07-16 12:13:08 +0200

    Program iterates over an array and execute a call back to a function using function pointer

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+# include "function_pointers.h"
+
+/**
+ * array_iterator - iterates a function on each element of an array
+ * @array: is a pointer to int
+ * @size: is the size of the array of int
+ * @action: function pointer: takes an int as argument
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int index;
+
+	while (index < size)
+	{
+		array[index] = action(array[index]);
+		index++;
+	}
+}
