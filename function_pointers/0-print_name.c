977c6a303e4f0d237a9b4841c6b1b6ad1e7dd1a3
bluelightwatcher <9550@holbertonstudents.com>
2024-07-16 12:09:31 +0200

    Using a func pointer to call back a function to print a name

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,30 @@
+# include "function_pointers.h"
+
+/**
+ * write - writes the variable name
+ * @name: is a pointer to char
+ * Return: void
+ */
+
+void write(char *name)
+{
+	while (*name != '\0')
+	{
+		_putchar(*name);
+		name++;
+	}
+	_putchar('\n');
+}
+
+/**
+ * print_name - prints a name by calling  back write function
+ * @name: is a pointer to char
+ * @f: function pointer: takes a pointer to char as argument
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f = &write;
+	f(name);
+}
