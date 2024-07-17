027177fa35eab050d7db0a54dbb43559facf7f44
bluelightwatcher <9550@holbertonstudents.com>
2024-07-17 09:16:32 +0200

    Removing custom function

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,22 +1,6 @@
 # include "function_pointers.h"
 
 /**
- * write - writes the variable name
- * @name: is a pointer to char
- * Return: void
- */
-
-void write(char *name)
-{
-	while (*name != '\0')
-	{
-		_putchar(*name);
-		name++;
-	}
-	_putchar('\n');
-}
-
-/**
  * print_name - prints a name by calling  back write function
  * @name: is a pointer to char
  * @f: function pointer: takes a pointer to char as argument
