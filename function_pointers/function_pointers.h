dc10c560e1bbfb086155543b18b2ba29ba83780f
bluelightwatcher <9550@holbertonstudents.com>
2024-07-17 10:21:03 +0200

    Updating header file

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+int int_index(int *array, int size, int (*cmp)(int));
 void array_iterator(int *array, size_t size, void (*action)(int)); /* array_iterator prototype */
 void print_name(char *name, void (*f)(char *)); /* print_name prototype */
 int _putchar(char c); /* putchar prototype */
