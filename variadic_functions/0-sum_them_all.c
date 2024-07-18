742700b91b6d1abbcfc0f26cab31fe7f5d7a2148
bluelightwatcher <9550@holbertonstudents.com>
2024-07-18 11:25:35 +0200

    Creating folder and adding sum em all program

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - variadic function of n elements
+ * @n: is the number of elements to access in the macro
+ * Return: sum of n elements
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum = 0;
+	int index = 0;
+	va_list suite;
+
+	va_start(suite, n);
+
+	if (n == 0)
+	{
+		return (0);
+	}
+
+	while (index < n)
+	{
+		sum = +va_arg(suite, int);
+	}
+	va_end(suite);
+	return (sum);
+}
+
---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,8 @@
+#ifndef VARIADIC_HEADER
+#define VARIADIC_HEADER
+#include<stdio.h>
+#include<stdarg.h>
+
+int sum_them_all(const unsigned int n, ...);
+
+#endif
