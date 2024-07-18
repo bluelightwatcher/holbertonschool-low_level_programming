dae281473a44faea7c46ca20f40c6fb81b727933
bluelightwatcher <9550@holbertonstudents.com>
2024-07-18 14:18:30 +0200

    Program prints separated strings

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,47 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_strings - variadic function of n elements
+ * @separator: is a pointer to char
+ * @n:is the number of elements to access in the macro
+ * Return: void
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	int index = 0;
+	va_list suite;
+	char *string;
+
+	va_start(suite, n);
+
+	if (n == 0)
+	{
+		exit(-1);
+	}
+
+	while (index < ((int)n))
+	{
+		string = (va_arg(suite, char*));
+		if (string)
+		{
+			printf("%s", string);
+		}
+		else
+		{
+			printf("%s", "(nil)");
+		}
+
+		if (separator != NULL && index < (int)n - 1)
+		{
+		printf("%s", separator);
+		}
+		index++;
+	}
+	va_end(suite);
+	putchar('\n');
+
+}
+
