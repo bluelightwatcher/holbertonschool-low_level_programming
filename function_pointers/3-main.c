bb2bb6f878c001991048f617506df2894d78e44f
bluelightwatcher <9550@holbertonstudents.com>
2024-07-19 17:25:34 +0200

    Program should run without the documentation

---
+++ function_pointers/3-main.c
@@ -0,0 +1,33 @@
+#include "3-calc.h"
+
+int main(int argc, char *argv[])
+{
+
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[3]);
+	char operator = argv[2][0];
+
+	int (*operation)(int, int) = get_op_func(argv[2]);
+	
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit (98);
+	}
+	
+	if (operator != '+' &&  operator != '-' && operator != '/' && operator != '*' && operator != '%')
+	{
+		printf("Error\n");
+		exit (99);
+	}
+	if ((operator == '/' || operator == '%') && num2 == '0')
+	{
+		printf("Error\n");
+		exit (100);
+	}
+	int result = operation(num1, num2);
+	printf("%d\n", result);
+	
+	return 0;
+}
