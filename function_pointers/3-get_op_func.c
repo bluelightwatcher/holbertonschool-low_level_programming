fcbdac2ea49f945a3163984ffa25d5af1784c6de
bluelightwatcher <9550@holbertonstudents.com>
2024-07-19 17:37:22 +0200

    Moving arrays of structure in header and initialiazing i in function body

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,39 +1,8 @@
 #include "3-calc.h"
 
-/**
- * _strcmp - loops through 2 strings simultaneously
- * Description: Compares ascii value of each characters for each increment
- * @s1: is a string
- * @s2: is a string
- * Return: the diffence between character value found in s1 and s2 (as ascii)
- */
-
-int _strcmp(char *s1, char *s2)
-{
-        while (*s1 != '\0' && *s2 != '\0')
-        {
-                if (*s1 != *s2)
-                {
-                        return (*s1 - *s2);
-                }
-        s1++;
-        s2++;
-        }
-return (*s1 - *s2);
-}
-
-op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i;
-
 int (*get_op_func(char *s))(int, int)
 {
+	int i = 0;
 	while (ops[i].op != NULL)
 	{
 		if (strcmp(s, ops[i].op) == 0)
