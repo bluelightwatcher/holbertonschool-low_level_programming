14c962f5fbf1ff4ce7356b529b568b68097d7eb7
bluelightwatcher <9550@holbertonstudents.com>
2024-07-29 14:28:44 +0200

    Folder initialization

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+/**
+ * print_list - prints element in a singly linked list
+ * @h: is a pointer to the list head node
+ * Return: number of elements (nodes) in the list
+ */
+
+size_t print_list(const list_t *h)
+{
+	unsigned int count = 0;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+		printf("%s\n", h->str);
+		}
+		count++;
+		h = h->next;
+	}
+
+return (count);
+}
