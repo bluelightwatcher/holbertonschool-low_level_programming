f3136c5439641d29fda1b9b781a080e2c1bf2a47
bluelightwatcher <9550@holbertonstudents.com>
2024-07-31 13:21:32 +0200

    Betty style applied

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -20,12 +20,12 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	}
 	new->n = n;
 	new->next = NULL;
-	
+
 	if (*head == NULL)
 	{
 		new->prev = NULL;
 		*head = new;
-		return(*head);
+		return (*head);
 	}
 
 	temp = *head;
