0914963130eb6b6f542791f3b52238d0d3ede7c7
bluelightwatcher <9550@holbertonstudents.com>
2024-07-31 14:38:30 +0200

    Programm calculates total sum of n in a list - header file updated -

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - function returns the sum of numbers in a list
+ * @head: is the start of list
+ * Return: unsigned int typedcast as an int 
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	unsigned int sum;
+
+	if (head == NULL)
+	{
+		return(0);
+	}
+	else
+	{
+		sum = 0;
+		while (head != NULL)
+		{
+			sum += head->n;
+			head = head->next;
+		}
+	}
+	return ((int)sum);
+}
