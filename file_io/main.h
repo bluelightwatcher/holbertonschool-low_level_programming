eb63fba18ddbbb165654bfb81b87d27f8724ea22
bluelightwatcher <9550@holbertonstudents.com>
2024-08-03 16:10:31 +0200

    Adding stddef.h

--- file_io/main.h
+++ file_io/main.h
@@ -1,6 +1,10 @@
 #ifndef FILEIO
 #define FILEIO
 
+#include <stdlib.h>
+#include <unistd>
+#include <fcntl.h>
+#include <stddef.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int _putchar(char c);
