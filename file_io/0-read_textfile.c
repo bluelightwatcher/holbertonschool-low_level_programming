e65363353a98145cabe58f5433d8078cd719233a
bluelightwatcher <9550@holbertonstudents.com>
2024-08-03 17:09:53 +0200

    Adjusting program to C90 standard

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -3,14 +3,15 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+
+	int fd;
+	ssize_t count;
 	char *buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 	{
 		return (0);
 	}
 
-	int fd;
-	ssize_t count;
 
 	/*edge case*/
 	if (filename == NULL)
