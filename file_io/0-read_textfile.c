cdb04684e1bb8d20a905f88bb125a2db53df96bd
bluelightwatcher <9550@holbertonstudents.com>
2024-08-04 11:22:49 +0200

    Adding write syscall

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -32,8 +32,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		free(buffer);
 		return (0);
 	}
+	write(STDOUT_FILENO, filename, count);
+
 free(buffer);
 close(fd);
 return (count);
 }
-
