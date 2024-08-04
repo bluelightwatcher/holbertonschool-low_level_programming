a107ec385c93683d9df011b0869f91a2837a544a
bluelightwatcher <9550@holbertonstudents.com>
2024-08-04 12:44:22 +0200

    Removing extra char

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -24,7 +24,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	count = sizeof(char) * (strlen(text_content) + 1);
+	count = sizeof(char) * (strlen(text_content));
 
 	if (!text_content)
 	{
